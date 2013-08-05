17921f46196ad9ff764e197628465ddee4ac0135
georgewsu <georgewsu@gmail.com>
2013-08-05 00:09:47 -0700

    Page to show columns on widget table.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,18 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import anorm.SQL
+import play.api.Play.current
+import play.api.db.DB
+import play.api.mvc.Action
+import play.api.mvc.Controller
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val columns = DB.withConnection { implicit c =>
+      SQL("show columns from widget")().map(row => (row[String]("COLUMNS.COLUMN_NAME"), row[String]("TYPE"))).toList
+    }
+    Ok(views.html.index(columns))
   }
-  
+
 }
